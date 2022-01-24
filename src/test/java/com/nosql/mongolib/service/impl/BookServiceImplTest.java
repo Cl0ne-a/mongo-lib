ac7b7f01e3bd265e918e0d7fc12f31b5c9b84cc1
Anna <unit10@protonmail.com>
2022-01-24 16:37:58 +0200

    added listener

--- src/test/java/com/nosql/mongolib/service/impl/BookServiceImplTest.java
+++ src/test/java/com/nosql/mongolib/service/impl/BookServiceImplTest.java
@@ -1,8 +1,30 @@
 package com.nosql.mongolib.service.impl;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
+@DataMongoTest
 class BookServiceImplTest extends MongoLibApplicationTest {
+
+
+    @Configuration
+    public static class MongoConfig{
+
+        public @Bean
+        MongoClient mongoclient() {
+            return MongoClients.create("mongodb://localhost:27017");
+        }
+
+        public @Bean
+        MongoTemplate mongoTemplate() {
+            return new MongoTemplate(mongoclient(), "nolibrary");
+        }
+    }
 
     @Test
     void findAllBooks() {
--- src/test/java/com/nosql/mongolib/service/impl/MongoLibApplicationTest.java
+++ src/test/java/com/nosql/mongolib/service/impl/MongoLibApplicationTest.java
@@ -1,10 +1,10 @@
 package com.nosql.mongolib.service.impl;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.context.annotation.ComponentScan;
 
-@DataMongoTest
+@SpringBootApplication
 @EnableConfigurationProperties
 @ComponentScan("com.nosql.mongolib.repository")
 public abstract class MongoLibApplicationTest {
