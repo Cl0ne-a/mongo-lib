67ad3bb1916dbee10a94cba5c04f2beca3fa93ea
Anna <unit10@protonmail.com>
2022-01-25 12:35:22 +0200

    added ShellUI

--- src/main/java/com/nosql/mongolib/MongoLibApplication.java
+++ src/main/java/com/nosql/mongolib/MongoLibApplication.java
@@ -1,12 +1,5 @@
 package com.nosql.mongolib;
 
-import com.nosql.mongolib.model.Author;
-import com.nosql.mongolib.model.Book;
-import com.nosql.mongolib.model.Genre;
-import com.nosql.mongolib.repository.GenreRepository;
-import com.nosql.mongolib.service.AuthorService;
-import com.nosql.mongolib.service.BookService;
-import lombok.val;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
